24901c1aafa4cb503a31a64490834656f3b4019c
shirakiya <shirakiya.pv@gmail.com>
2019-05-23 02:32:03 +0900

    add polyfill for Google bot

--- webpack.common.js
+++ webpack.common.js
@@ -42,8 +42,14 @@ module.exports = {
             [
               '@babel/preset-env',
               {
+                targets: {
+                  browsers: [
+                    'last 2 versions',
+                    'Chrome >= 41',
+                  ],
+                },
                 modules: false,
-                useBuiltIns: 'usage'
+                useBuiltIns: 'usage',
               },
             ],
           ],
